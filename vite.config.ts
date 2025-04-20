6c016b05f63899f3f7c227da401d135a0e1e6184
yingyingyingqwq <70638453+yingyingyingqwq@users.noreply.github.com>
2025-04-20 11:13:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: "/PakaNews/",
+    base: "",
     build: {
         rollupOptions: {
             input: {
